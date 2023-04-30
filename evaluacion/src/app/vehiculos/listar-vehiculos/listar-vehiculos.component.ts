a726e035e8a69f986b1c4f1534ea042c48156240
Daniel Jimenez <d.jimenez112@uniandes.edu.co>
2023-04-30 00:16:04 -0600

    Punto 2 agregar calculo de cantidad de vehiculos por marca.

--- evaluacion/src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
+++ evaluacion/src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
@@ -9,11 +9,34 @@ import { Vehiculo } from 'src/app/models/vehiculo.model';
 })
 export class ListarVehiculosComponent implements OnInit{
   public vehiculos:Vehiculo[] = [];
+  public vehiculosPorMarca: Array<{marca:string, cantidad:number}>=[]
 constructor(private vehiculosService:VehiculosService){}
   ngOnInit(): void {
-    this.vehiculosService.getVehicles().subscribe(vehiculos =>{
-      this.vehiculos = vehiculos;
-    })
+    this.getVehiculos();
   }
 
+
+  private getVehiculos() {
+    this.vehiculosService.getVehicles().subscribe(vehiculos => {
+      this.vehiculos = vehiculos;
+
+      this.calcularCantidadPorMarca();
+    });
+  }
+
+  private calcularCantidadPorMarca() {
+    let arrayInicial: Array<{marca:string, cantidad:number}> = [];
+    this.vehiculosPorMarca = this.vehiculos.map(vehiculo => ({ marca: vehiculo.marca, cantidad:1 })).reduce((acc, marca) => {
+      let marcaA = acc.find(value => value.marca == marca.marca);
+      if (marcaA == undefined) {
+        acc.push({
+          marca: marca.marca,
+          cantidad: 1
+        });
+      } else {
+        marcaA.cantidad = marcaA.cantidad + 1;
+      }
+      return acc;
+    }, arrayInicial);
+  }
 }
