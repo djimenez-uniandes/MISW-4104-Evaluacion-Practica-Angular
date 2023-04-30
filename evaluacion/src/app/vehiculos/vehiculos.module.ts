f08b7caff6c47f69ab1b7ba2b586032f4fafc718
Daniel Jimenez <d.jimenez112@uniandes.edu.co>
2023-04-29 23:45:01 -0600

    Agregando Rutas locales para asegurar navegacion

--- evaluacion/src/app/vehiculos/vehiculos.module.ts
+++ evaluacion/src/app/vehiculos/vehiculos.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarVehiculosComponent } from './listar-vehiculos/listar-vehiculos.component';
 import { VehiculosRoutingModule } from './vehiculos-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -11,7 +12,7 @@ import { VehiculosRoutingModule } from './vehiculos-routing.module';
   ],
   imports: [
     CommonModule,
-    VehiculosRoutingModule
+    VehiculosRoutingModule,
   ]
 })
 export class VehiculosModule { }
