69e1fed7aab28efa77169b796fefa04089339b33
Daniel Jimenez <d.jimenez112@uniandes.edu.co>
2023-04-29 23:45:49 -0600

    Codificar logia que lista vehiculos cierre punto 1

--- evaluacion/src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
+++ evaluacion/src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VehiculosService } from '../vehiculos.service';
+import { Vehiculo } from 'src/app/models/vehiculo.model';
 
 @Component({
   selector: 'app-listar-vehiculos',
   templateUrl: './listar-vehiculos.component.html',
   styleUrls: ['./listar-vehiculos.component.scss']
 })
-export class ListarVehiculosComponent {
+export class ListarVehiculosComponent implements OnInit{
+  public vehiculos:Vehiculo[] = [];
+constructor(private vehiculosService:VehiculosService){}
+  ngOnInit(): void {
+    this.vehiculosService.getVehicles().subscribe(vehiculos =>{
+      this.vehiculos = vehiculos;
+    })
+  }
 
 }
