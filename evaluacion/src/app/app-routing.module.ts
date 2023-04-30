9bacd1e56e0e49ba4183ab0d9c91192bd4065b19
Daniel Jimenez <d.jimenez112@uniandes.edu.co>
2023-04-29 23:25:30 -0600

    Path default redirige a /vehiculos

--- evaluacion/src/app/app-routing.module.ts
+++ evaluacion/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'vehiculos' },
   {
     path: 'vehiculos',
     loadChildren: () => import('./vehiculos/vehiculos.module').then(m => m.VehiculosModule)
